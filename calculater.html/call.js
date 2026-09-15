af9d10327e4a6e1eeec6223413c129b98a47b44e
yavraj <yuvrajmaurya85420@gmail.com>
2026-09-15 15:03:26 +0530

    calculator practice

---
+++ calculater.html/call.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+
+function append(value) {
+  if (display.innerText === '0') {
+    display.innerText = value;
+  } else {
+    display.innerText += value;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = '0';
+}
+
+function calculate() {
+  try {
+    display.innerText = eval(display.innerText);
+  } catch (e) {
+    display.innerText = 'Error';
+  }
+}
